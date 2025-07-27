8a88fd5c3dd3000db83107f5a7aac4856717ecc8
ShahidaKhan1 <shahida.2021eee@gmail.com>
2025-07-28 01:34:00 +0530

    test_homepagechanges

--- Scripts/test_homepage.py
+++ Scripts/test_homepage.py
@@ -1,18 +1,34 @@
 import time
 import sys
 import os
+# import pytest
+
 
 # Add parent directory to path to allow imports
 sys.path.append(os.path.abspath(".."))
 
+from POM.login import Login
 from POM.home_page import HomePage
 from generic_utilities.excel_utility import excel_data
 
-data = excel_data('Product_Sheet')
+login_data = excel_data('Sheet1')
+product_data = excel_data('Product_Sheet')
 
-def test_homepage(browser_setup):
+
+def test_login_and_homepage(browser_setup):
+
+    #Step 1: Login
+    login_obj = Login(browser_setup)
+    login_obj.click_on_login()
+    login_obj.enter_login_email(login_data['login_email'])
+    login_obj.click_continue()
+    login_obj.enter_login_pwd(login_data['login_pwd'])
+    login_obj.click_signin()
+    time.sleep(3)
+
+    # Step 2: Home Page actions
     homepage_obj = HomePage(browser_setup)
-    homepage_obj.search_product(data['prod1'])
+    homepage_obj.search_product(product_data['prod1'])
     time.sleep(2)
     homepage_obj.search_button()
     time.sleep(2)
